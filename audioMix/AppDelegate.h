ffb3d3df6d374159e3942933ecc1941b4176cdd8
Lightning <lightning@CaptaindeMac-mini.local>
2015-05-15 09:34:26 +0800

    Initial Commit

---
+++ audioMix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  audioMix
+//
+//  Created by Lightning on 15/5/15.
+//  Copyright (c) 2015年 Lightning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ audioMix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  audioMix
+//
+//  Created by Lightning on 15/5/15.
+//  Copyright (c) 2015年 Lightning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
